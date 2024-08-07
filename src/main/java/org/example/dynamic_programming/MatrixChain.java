5e0cc39979707d433f65a258bad59e87a890b8be
wangyong <writeletters@qq.com>
2024-08-07 12:50:48 +0800

    dynamic programming: MatrixChain(iterative)

--- src/main/java/org/example/dynamic_programming/MatrixChain.java
+++ src/main/java/org/example/dynamic_programming/MatrixChain.java
@@ -27,4 +27,33 @@ public class MatrixChain {
         return mIJ;
     }
 
+    public static int iterative(int[] vectorP) {
+        int n = vectorP.length - 1;
+        int[][] m = new int[n + 1][n + 1];
+        int[][] s = new int[n + 1][n + 1];
+
+        for (int r = 2; r <= n; r++) { // r个矩阵连乘的子问题
+            for (int i = 1; i <= n - r + 1; i++) { // Ai * Ai+1 * ... * Ai+r-1
+                int j = i + r - 1; // Ai * Ai+1 * ... * Aj
+
+                m[i][j] = m[i][i] + m[i + 1][j] + vectorP[i - 1] * vectorP[i] * vectorP[j];
+                s[i][j] = i;
+
+                for (int k = i +1; k < j; k++) { // (Ai * Ai+1 * ... * Ak) * (Ak+1 * Ak+2 * ... * Aj)
+                    int mIKJ = m[i][k] + m[k+1][j] + vectorP[i - 1] * vectorP[k] * vectorP[j];
+                    if (mIKJ < m[i][j]) {
+                        m[i][j] = mIKJ;
+                        s[i][j] = k;
+                    }
+                }
+            }
+        }
+
+        System.out.printf("m[%s, %s]=%s%n", 1, n, m[1][n]);
+        System.out.printf("s[%s, %s]=%s%n", 1, n, s[1][n]);
+        System.out.println("----------");
+
+        return m[1][n];
+    }
+
 }
