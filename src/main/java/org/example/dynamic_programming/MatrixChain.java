b6ece7b48f0c7a0cbe6a19ef688934c5ae5fc307
wangyong <writeletters@qq.com>
2024-08-07 13:02:39 +0800

    dynamic programming: perfect MatrixChain

--- src/main/java/org/example/dynamic_programming/MatrixChain.java
+++ src/main/java/org/example/dynamic_programming/MatrixChain.java
@@ -2,7 +2,7 @@ package org.example.dynamic_programming;
 
 public class MatrixChain {
 
-    public static int recursive(int[] vectorP, int i, int j) {
+    public static int recursive(int[] vectorP, int[][] m, int[][] s, int i, int j) {
         if (i == j) {
             return 0;
         }
@@ -11,7 +11,7 @@ public class MatrixChain {
         int sIJ = i;
 
         for (int k = i; k < j; k++) {
-            int mIKJ = recursive(vectorP, i, k) + recursive(vectorP, k + 1, j) + vectorP[i - 1] * vectorP[k] * vectorP[j];
+            int mIKJ = recursive(vectorP, m, s, i, k) + recursive(vectorP, m, s, k + 1, j) + vectorP[i - 1] * vectorP[k] * vectorP[j];
 
             if (mIJ == -1 || mIKJ < mIJ) {
                 mIJ = mIKJ;
@@ -19,19 +19,16 @@ public class MatrixChain {
             }
         }
 
-
-        System.out.printf("m[%s, %s]=%s%n", i, j, mIJ);
-        System.out.printf("s[%s, %s]=%s%n", i, j, sIJ);
+        m[i][j] = mIJ;
+        s[i][j] = sIJ;
+        System.out.printf("m[%s, %s]=%s%n", i, j, m[i][j]);
+        System.out.printf("s[%s, %s]=%s%n", i, j, s[i][j]);
         System.out.println("----------");
 
-        return mIJ;
+        return m[i][j];
     }
 
-    public static int iterative(int[] vectorP) {
-        int n = vectorP.length - 1;
-        int[][] m = new int[n + 1][n + 1];
-        int[][] s = new int[n + 1][n + 1];
-
+    public static int iterative(int[] vectorP, int[][] m, int[][] s, int n) {
         for (int r = 2; r <= n; r++) { // r个矩阵连乘的子问题
             for (int i = 1; i <= n - r + 1; i++) { // Ai * Ai+1 * ... * Ai+r-1
                 int j = i + r - 1; // Ai * Ai+1 * ... * Aj
