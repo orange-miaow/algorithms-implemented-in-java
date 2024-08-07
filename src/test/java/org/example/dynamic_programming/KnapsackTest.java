2fbf64e35cecf0bce5de5d2699331f061c2ea984
wangyong <writeletters@qq.com>
2024-08-08 00:29:36 +0800

    dynamic programming: Knapsack(iterative)

---
+++ src/test/java/org/example/dynamic_programming/KnapsackTest.java
@@ -0,0 +1,32 @@
+package org.example.dynamic_programming;
+
+import org.junit.jupiter.api.Test;
+
+class KnapsackTest {
+
+    int n = 4;
+    int b = 10;
+    int[] w = new int[]{0, 2, 3, 4, 7};
+    int[] v = new int[]{0, 1, 3, 5, 9};
+    int[][] F = new int[n + 1][b + 1];
+    {
+        for (int j = 0; j < F.length; j++) {
+            F[j][0] = 0;
+        }
+
+        for (int j = 0; j < F[0].length; j++) {
+            F[0][j] = 0;
+        }
+
+        for (int j = 1; j < F[0].length; j++) {
+            F[1][j] = (int) Math.floor((double) j / w[1]) * v[1];
+        }
+
+    }
+
+    @Test
+    void iterative() {
+        Knapsack.iterative(n, b, w, v, F);
+        System.out.printf("F[%s][%s]=%s\n", n, b, F[n][b]);
+    }
+}
