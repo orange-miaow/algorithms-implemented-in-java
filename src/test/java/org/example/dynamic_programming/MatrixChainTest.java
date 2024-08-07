d941e43e21aafccf3e056fdb434ff84bc6b9a97d
orange-miaow <writeletters@qq.com>
2024-08-07 13:02:39 +0800

    dynamic programming: perfect MatrixChain

--- src/test/java/org/example/dynamic_programming/MatrixChainTest.java
+++ src/test/java/org/example/dynamic_programming/MatrixChainTest.java
@@ -8,7 +8,11 @@ class MatrixChainTest {
 
     @Test
     void recursive() {
-        MatrixChain.recursive(vectorP, 1, 5);
-        MatrixChain.iterative(vectorP);
+        int n = vectorP.length - 1;
+        int[][] m = new int[n + 1][n + 1];
+        int[][] s = new int[n + 1][n + 1];
+        MatrixChain.recursive(vectorP, m, s, 1, 5);
+
+        MatrixChain.iterative(vectorP, m, s, n);
     }
 }
