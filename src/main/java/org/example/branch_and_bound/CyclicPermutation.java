247788aee8e8d53ec003df55ed44ec27229e0b6a
orange-miaow <writeletters@qq.com>
2024-08-31 13:16:51 +0800

    branch-and-bound: perfect CyclicPermutation, correct mistakes

--- src/main/java/org/example/branch_and_bound/CyclicPermutation.java
+++ src/main/java/org/example/branch_and_bound/CyclicPermutation.java
@@ -10,10 +10,12 @@ public class CyclicPermutation {
     // [i] 圆i的半径
     int[] r;
 
-    // 界函数值 1表示正无穷
+    // 界函数值 -1表示正无穷
     private double lowerBoundFunctionValue;
     // 解向量
     private int[] solutionVector;
+    // 解向量所对应的各个圆的圆心横坐标
+    private double[] solutionCentres;
 
     public CyclicPermutation(int[] r) {
         this.r = r;
@@ -30,14 +32,18 @@ public class CyclicPermutation {
         return solutionVector;
     }
 
+    public double[] getSolutionCentres() {
+        return solutionCentres;
+    }
+
     /**
      * 在搜索树中搜寻最优解
      *
-     * @param level      层级 level=0,1,...,n-1
-     * @param vector     根结点至当前结点的向量
-     * @param lastCycleX 第level-1个位置所放的圆的圆心横坐标
+     * @param level   层级 level=0,1,...,n-1
+     * @param vector  根结点至当前结点的向量
+     * @param centres 根结点至当前结点所放的圆的圆心横坐标
      */
-    public void searchOptimalSolution(int level, int[] vector, double lastCycleX) {
+    public void searchOptimalSolution(int level, int[] vector, double[] centres) {
         // 访问状态数组
         int[] visitedStateArray = new int[n];
         for (int i = 0; i < level; i++) {
@@ -68,20 +74,16 @@ public class CyclicPermutation {
 
             // 第level个位置所放的圆的圆心横坐标
             // 规定第0个位置所放的圆的圆心为坐标原点
-            double currentCycleX = 0;
-            if (level != 0) {
-                // 第level个位置所放的圆的圆心横坐标与第level-1个位置所放的圆的圆心横坐标的差
-                double d = 2 * Math.sqrt(r[vector[level - 1]] * r[vector[level]]);
-                currentCycleX = lastCycleX + d;
-            }
+            centres[level] = calculateX(level, vector, centres);
 
             if (level == n - 1) {
-                // 已到达叶节点 即最后一个圆
+                // 已到达叶结点 即最后一个圆
                 // 计算排列长度
-                double l = currentCycleX + r[vector[0]] + r[vector[level]];
+                double l = calculateL(vector, centres);
                 if ((lowerBoundFunctionValue == -1) || (l < lowerBoundFunctionValue)) {
                     lowerBoundFunctionValue = l;
                     solutionVector = vector.clone();
+                    solutionCentres = centres.clone();
                 }
                 continue;
             }
@@ -89,16 +91,62 @@ public class CyclicPermutation {
             // 计算代价函数值
             // 求第level个位置以及后面待选的n-1-level个位置所放的圆中最小半径的值
             // 使用预计算值minRadius即可
-            double costFunctionValue = currentCycleX + (2 * n - 2 * (level + 1) + 1) * minRadius + r[vector[0]];
+            double costFunctionValue = centres[level] + (2 * n - 2 * (level + 1) + 1) * minRadius + r[vector[0]];
 
             if ((lowerBoundFunctionValue == -1) || (costFunctionValue < lowerBoundFunctionValue)) {
                 // 根据深度优先策略(DFS)遍历该结点的下一个结点
-                searchOptimalSolution(level + 1, vector, currentCycleX);
+                searchOptimalSolution(level + 1, vector, centres);
             } else {
                 // 剪枝
                 // 由该结点回溯至其父结点 之后根据深度优先策略(DFS)遍历父结点的下一个可用结点
             }
         }
+    }
+
+    // 计算第level个位置所放的圆的圆心横坐标
+    private double calculateX(int level, int[] vector, double[] centres) {
+        double x = 0;
+        int radius = r[vector[level]];
+
+        for (int i = 0; i < level; i++) {
+            double xi = centres[i];
+            int ri = r[vector[i]];
+
+            // 第level个位置所放的圆与第i个位置所放的圆相切时的圆心横坐标的差
+            double d = 2 * Math.sqrt(radius * ri);
+            double tempX = xi + d;
+            if (tempX > x) {
+                // 移动第level个位置所放的圆 使其与第i个位置所放的圆相切
+                x = tempX;
+            }
+        }
+
+        return x;
+    }
+
+    // 计算排列长度
+    private double calculateL(int[] vector, double[] centres) {
+        // 排列左边界
+        double left = centres[0] - r[vector[0]];
+        // 排列右边界
+        double right = centres[0] + r[vector[0]];
+
+        for (int i = 1; i <= n - 1; i++) {
+            // 第i个位置所放的圆的左边界
+            double currentLeft = centres[i] - r[vector[i]];
+            // 第i个位置所放的圆的右边界
+            double currentRight = centres[i] + r[vector[i]];
+
+            if (currentLeft < left) {
+                left = currentLeft;
+            }
+
+            if (currentRight > right) {
+                right = currentRight;
+            }
+        }
+
+        return (right - left);
     }
 
 }
--- src/test/java/org/example/branch_and_bound/CyclicPermutationTest.java
+++ src/test/java/org/example/branch_and_bound/CyclicPermutationTest.java
@@ -11,11 +11,13 @@ class CyclicPermutationTest {
     @Test
     void searchOptimalSolution() {
         CyclicPermutation cyclicPermutation = new CyclicPermutation(r);
-        cyclicPermutation.searchOptimalSolution(0, new int[r.length], 0);
+        cyclicPermutation.searchOptimalSolution(0, new int[r.length], new double[r.length]);
 
         double lowerBoundFunctionValue = cyclicPermutation.getLowerBoundFunctionValue();
         int[] solutionVector = cyclicPermutation.getSolutionVector();
+        double[] solutionCentres = cyclicPermutation.getSolutionCentres();
         System.out.printf("最小排列长度: %s\n", lowerBoundFunctionValue);
         System.out.printf("最优解向量: %s\n", Arrays.toString(solutionVector));
+        System.out.printf("最优解向量所对应的各个圆的圆心横坐标: %s\n", Arrays.toString(solutionCentres));
     }
 }
